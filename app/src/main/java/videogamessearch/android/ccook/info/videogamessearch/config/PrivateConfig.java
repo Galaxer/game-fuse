181a2bbee9a9d3d3b4b52b18ee9fc5e3c34c209c
Chris Cook <mintopher@gmail.com>
2016-06-02 17:20:17 -0400

    Decided to leave API key in code since it's not highly sensitive
    - Remove PrivateConfig from gitignore
    - Add API key to PrivateConfig

---
+++ app/src/main/java/videogamessearch/android/ccook/info/videogamessearch/config/PrivateConfig.java
@@ -0,0 +1,5 @@
+package videogamessearch.android.ccook.info.videogamessearch.config;
+
+public class PrivateConfig {
+    public static final String GIANT_BOMB_API_KEY = "40133e16522fcb9bd83476131914800bf44347a5";
+}
